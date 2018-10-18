76b70555ef6289818d7f709c99b9ed9066486e71
iamp15 <44248881+iamp15@users.noreply.github.com>
2018-10-18 00:46:04 -0400

    version 1.0.1

--- src/mainPackage/Memoria.java
+++ src/mainPackage/Memoria.java
@@ -7,18 +7,20 @@ import java.io.*;
 
 public class Memoria {
     
+  
+    
     public static void saveCount(int conteo) throws IOException{
-        String data =Integer.toString(conteo);           
-            BufferedWriter wBuff = new BufferedWriter(new FileWriter("C:\\Users\\iamp1\\OneDrive\\Java\\Practicas\\IAMP_SG\\conteo.txt"));
-            wBuff.write(data);
-            wBuff.close();
-        
-        
+        String data =Integer.toString(conteo);
+        System.out.println();
+        BufferedWriter wBuff = new BufferedWriter(new FileWriter("conteo.txt"));
+        wBuff.write(data);
+        wBuff.close();
     }
     
     public static String loadCount() throws IOException{
+        
      
-        BufferedReader rBuff = new BufferedReader(new FileReader("C:\\Users\\iamp1\\OneDrive\\Java\\Practicas\\IAMP_SG\\conteo.txt"));
+        BufferedReader rBuff = new BufferedReader(new FileReader("conteo.txt"));
         String linea = rBuff.readLine();
         rBuff.close();
         
